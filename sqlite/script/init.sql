56b48be1b00889a8c6ef8838fb6dc0341e6b5c21
zouguodong <zouguodong@mampod.com>
2022-08-28 16:16:56 +0800

    update: update init.sql

--- sqlite/script/init.sql
+++ sqlite/script/init.sql
@@ -7,17 +7,47 @@ create table blog
     like_count    integer not null default 0,
     collect_count integer not null default 0,
     scores        text    not null default '',
-    created_at    integer not null default strftime('%s'),
-    updated_at    integer not null default strftime('%s'),
+    created_at    integer not null default CURRENT_TIMESTAMP,
+    updated_at    integer not null default CURRENT_TIMESTAMP,
     deleted_at    integer
+);
+create table category
+(
+    id          integer not null primary key autoincrement,
+    name        text    not null,
+    description text,
+    created_at  integer not null default CURRENT_TIMESTAMP,
+    updated_at  integer not null default CURRENT_TIMESTAMP,
+    deleted_at  integer
+
+);
+create table blog_category
+(
+    blog_id     integer not null,
+    category_id integer not null,
+    created_at  integer not null default CURRENT_TIMESTAMP
+);
+create table label
+(
+    id         integer not null primary key autoincrement,
+    name       text    not null,
+    created_at integer not null default CURRENT_TIMESTAMP,
+    updated_at integer not null default CURRENT_TIMESTAMP,
+    deleted_at integer
+);
+create table blog_label
+(
+    blog_id    integer not null,
+    label_id   integer not null,
+    created_at integer not null default CURRENT_TIMESTAMP
 );
 create table visitor
 (
     id         integer not null primary key autoincrement,
     name       text    not null,
     email      text    not null,
-    created_at integer not null default strftime('%s'),
-    updated_at integer not null default strftime('%s'),
+    created_at integer not null default CURRENT_TIMESTAMP,
+    updated_at integer not null default CURRENT_TIMESTAMP,
     deleted_at integer
 );
 create table comment
@@ -29,7 +59,7 @@ create table comment
     g          integer not null,
     content    text    not null,
     approved   integer not null default 0,
-    created_at integer not null default strftime('%s'),
-    updated_at integer not null default strftime('%s'),
+    created_at integer not null default CURRENT_TIMESTAMP,
+    updated_at integer not null default CURRENT_TIMESTAMP,
     deleted_at integer
 );
